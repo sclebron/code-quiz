33edb0eb566f98b0036f0c351c7f4c42af96318b
SLeBron <sophieclebron@gmail.com>
2022-09-05 19:25:07 -0600

    began adding functions for timer

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let questions = [{
+let counterElement = document.getElementById("counter");
+
+function timer() {
+    let timeLeft;
+    document.getElementById("startbutton")
+    if (timeLeft === 0) {
+        endQuiz()l
+    } else {
+        counterElement.innerHTML = timeLeft + " seconds left";
+    }
+}
+
+function timeSubtract () {
+    timeLeft = timeLeft - 5;
+}
+
+function startQuiz() {
     
-}]
+}
